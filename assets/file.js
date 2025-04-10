4d743efb20e3b72218c8b4b3b4d8abbcc6b9b695
AboZekry2040 <mohamedzekry2040@gmail.com>
2025-04-10 15:07:48 +0200

    downlowd all file with img at assetsFolder

---
+++ assets/file.js
@@ -0,0 +1,15 @@
+function angularJs(){
+
+// let Index=0
+// document.querySelectorAll('.moveWord')[Index].classList.add('ActiveMoveWord')
+// setInterval(typingText(),6050)
+
+// function typingText(){
+//     document.querySelectorAll('.moveWord').forEach((e)=>{e.classList.remove('.ActiveMoveWord')})
+// Index++;
+// document.querySelectorAll('.moveWord')[Index].classList.add('ActiveMoveWord')
+// if(Index<=3){Index=0}
+// }
+
+
+}
